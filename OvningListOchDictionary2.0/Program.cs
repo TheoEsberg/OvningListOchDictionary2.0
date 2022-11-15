fb29df7156f942aad37dbc79208901866537828c
Theo Esberg <esbergtheo@gmail.com>
2022-11-15 14:45:14 +0100

    YES

--- OvningListOchDictionary2.0/Program.cs
+++ OvningListOchDictionary2.0/Program.cs
@@ -18,16 +18,23 @@ namespace OvningListOchDictionary2._0
 
             List<Country> countries = new List<Country>();
             countries.Add(new Country("Sweden", "SE", "Stockholm"));
-            countries.Add(new Country("Denmark", "DE", "Copenhagen"));
+            countries.Add(new Country("Denmark", "DK", "Copenhagen"));
             countries.Add(new Country("United States", "US", "Washington"));
             countries.Add(new Country("Spain", "ES", "Madrid"));
             countries.Add(new Country("Japan", "JP", "Tokyo"));
-            
-            string UCC = Console.ReadLine();
 
+            string UCC = Console.ReadLine().ToUpper();
+
+            int i = 0;
             foreach (Country country in countries) {
+                i++;
                 if (country.Code == UCC) {
                     Console.WriteLine("The country code {0} is the code of {1}", country.Code, country.Name);
+                    return;
+                }
+
+                if (i == countries.Count) {
+                    Console.WriteLine("The country code was not in the list!");
                 }
             }
         }
