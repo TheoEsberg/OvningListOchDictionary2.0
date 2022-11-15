ccd24468ece4fb2fd92ff52db2a4cc9797c740d7
Theo Esberg <esbergtheo@gmail.com>
2022-11-15 14:28:21 +0100

    Undo

--- OvningListOchDictionary2.0/Program.cs
+++ OvningListOchDictionary2.0/Program.cs
@@ -13,17 +13,15 @@ namespace OvningListOchDictionary2._0
 
         public static void App()
         {
-            InitCountryList();
-        }
 
-        public static void InitCountryList()
-        {
+
             List<Country> countries = new List<Country>();
             countries.Add(new Country("Sweden", "SE", "Stockholm"));
             countries.Add(new Country("Denmark", "DE", "Copenhagen"));
             countries.Add(new Country("United States", "US", "Washington"));
             countries.Add(new Country("Spain", "ES", "Madrid"));
             countries.Add(new Country("Japan", "JP", "Tokyo"));
+
         }
     }
 }
