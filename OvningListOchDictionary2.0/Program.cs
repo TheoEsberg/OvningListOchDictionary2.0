b8344f066d77619dfc98842483cceb8af2d41f6e
Patrik Skattberg <patrikskattberg@hotmail.com>
2022-11-15 14:45:40 +0100

    LoopThatMightWork

--- OvningListOchDictionary2.0/Program.cs
+++ OvningListOchDictionary2.0/Program.cs
@@ -14,22 +14,29 @@ namespace OvningListOchDictionary2._0
 
         public static void App()
         {
-
-
+            string PlayAgain;
             List<Country> countries = new List<Country>();
             countries.Add(new Country("Sweden", "SE", "Stockholm"));
             countries.Add(new Country("Denmark", "DE", "Copenhagen"));
             countries.Add(new Country("United States", "US", "Washington"));
             countries.Add(new Country("Spain", "ES", "Madrid"));
             countries.Add(new Country("Japan", "JP", "Tokyo"));
-            
-            string UCC = Console.ReadLine();
+            do
+            {
+                string UCC = Console.ReadLine();
 
-            foreach (Country country in countries) {
-                if (country.Code == UCC) {
-                    Console.WriteLine("The country code {0} is the code of {1}", country.Code, country.Name);
+                foreach (Country country in countries)
+                {
+                    if (country.Code == UCC)
+                    {
+                        Console.WriteLine("The country code {0} is the code of {1}", country.Code, country.Name);
+                    }
                 }
-            }
+
+                Console.WriteLine("Would you like to try agian?");
+                PlayAgain = Console.ReadLine().ToUpper();
+            } while (PlayAgain == "YES" || PlayAgain == "YE");
+
         }
     }
 }
